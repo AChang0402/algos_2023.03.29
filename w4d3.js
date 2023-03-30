255580a20828dd32abeddfd51f44ac0aa24a8200
AChang0402 <alexchang.0402@gmail.com>
2023-03-30 00:46:52 -0700

    first commit

---
+++ w4d3.js
@@ -0,0 +1,61 @@
+/* 
+  Return the fibonacci number at the nth position, recursively.
+  
+  Fibonacci sequence: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+  The next number is found by adding up the two numbers before it,
+  starting with 0 and 1 as the first two numbers of the sequence.
+*/
+
+const num1 = 0;
+const expected1 = 0;
+
+const num2 = 1;
+const expected2 = 1;
+
+const num3 = 2;
+const expected3 = 1;
+
+const num4 = 3;
+const expected4 = 2;
+
+const num5 = 4;
+const expected5 = 3;
+
+const num6 = 8;
+const expected6 = 21;
+
+
+function fib(num, count=1, num1=0, num2=1){
+  if(num<1){
+    return 0
+  }
+  if(count<num){
+    return fib(num,count+1, num2, num1+num2)
+  }
+  return num2
+}
+
+console.log(fib(0))
+console.log(fib(1))
+console.log(fib(2))
+console.log(fib(3))
+console.log(fib(4))
+console.log(fib(5))
+console.log(fib(6))
+console.log(fib(7))
+console.log(fib(8))
+console.log(fib(9))
+console.log(fib(10))
+console.log(fib(11))
+console.log(fib(12))
+console.log(fib(13))
+
+
+
+// 1. Driver 🚗
+// 2. Presenter 👩‍💻
+// 3. Navigator 🧭
+
+// take 5-8 mins to write the pseudocode here:
+
+// create the function and decide what params it needs and what it will return
